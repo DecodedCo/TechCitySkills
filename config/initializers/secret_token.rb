130dd92b1f47482d7213df1bf589dd574b23f0f9
Tom Wilshere <tom.wilshere@gmail.com>
2014-03-29 11:57:55 +0000

    Initial commit of Rails boilerplate

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TechCitySkills::Application.config.secret_token = '1ff6b8ec87db0d0f127738d2695d9a7327ab7b836bea0786426032bcb34d8e4e2452e02c9369a2056fc21b45a2879cda31c645fe360a9ae28919ddfecd4f03bb'
