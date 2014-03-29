2ce9e2d8fde4e29f6e8cd922d4a30e59b649fc92
Tom Wilshere <tom.wilshere@gmail.com>
2014-03-29 16:53:13 +0000

    image url additions

--- app/models/opportunity.rb
+++ app/models/opportunity.rb
@@ -1,3 +1,3 @@
 class Opportunity < ActiveRecord::Base
-  attr_accessible :address, :cost, :description, :girls_only, :into_work, :lat, :level_age_max, :level_age_min, :level_exp, :lon, :name, :site_url, :skills, :cost_min, :audience, :audience, :employer_opp, :lo_type
+  attr_accessible :address, :cost, :description, :girls_only, :into_work, :lat, :level_age_max, :level_age_min, :level_exp, :lon, :name, :site_url, :skills, :cost_min, :audience, :audience, :employer_opp, :lo_type, :image_url, :like_count
 end
